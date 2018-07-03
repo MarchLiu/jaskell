6a195d1acbdba9f71412f1af7a6361311f261593
Mars Liu <mars.liu@outlook.com>
2018-07-03 22:25:34 +0800

    add slurp and edn reader

---
+++ src/main/java/jaskell/util/CR.java
@@ -0,0 +1,42 @@
+package jaskell.util;
+
+import clojure.lang.IFn;
+import clojure.lang.RT;
+import clojure.lang.Var;
+
+public class CR {
+    static private IFn req = RT.var("clojure.core", "require");
+    static private IFn apply = RT.var("clojure.core", "apply");
+    static private IFn slurpFn = RT.var("clojure.core", "slurp");
+    static {
+        req.invoke(RT.readString("clojure.edn"));
+    }
+    static public void require(String namespace) {
+        req.invoke(RT.readString(namespace));
+    }
+
+    static public Object read(String code){
+        return RT.readString(code);
+    }
+
+    static public Object invoke(String namespace, String name, Object ... args) {
+        IFn func = RT.var(namespace, name).fn();
+        return apply.invoke(func, args);
+    }
+
+    static public Var var(String namespace, String name) {
+        return RT.var(namespace, name);
+    }
+
+    static public Object readEdn(String edn){
+        return RT.var("clojure.edn", "read-string").fn().invoke(edn);
+    }
+
+    static public Object loadEdn(String path){
+        return  readEdn(slurp(path));
+    }
+
+    static public String slurp(String path){
+        return (String)slurpFn.invoke(path);
+    }
+}
--- src/test/java/jasskell/util/ClojureTest.java
+++ src/test/java/jasskell/util/ClojureTest.java
@@ -1,20 +1,26 @@
 package jasskell.util;
 
 import clojure.lang.IFn;
+import clojure.lang.PersistentArrayMap;
 import clojure.lang.RT;
-import jaskell.util.ClojureRuntime;
+import jaskell.util.CR;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ClojureTest {
     @Test
     public void unionTest(){
-        ClojureRuntime runtime = new ClojureRuntime();
-        runtime.require("clojure.set");
-        IFn join = runtime.var("clojure.set", "union");
-        Object x = runtime.read("#{:a :b :c}");
-        Object y = runtime.read("#{:d}");
+        CR.require("clojure.set");
+        IFn join = CR.var("clojure.set", "union");
+        Object x = CR.read("#{:a :b :c}");
+        Object y = CR.read("#{:d}");
         Object z = join.invoke(x, y);
         Assert.assertEquals(z, RT.readString("#{:a :b :c :d}"));
+    }
+
+    @Test
+    public void slurpTest(){
+        PersistentArrayMap config = (PersistentArrayMap)CR.loadEdn("src/test/test.edn");
+        Assert.assertEquals(config.get(RT.readString(":path")), "/project/root");
     }
 }
